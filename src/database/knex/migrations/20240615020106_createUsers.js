b3aafa63a651fba8ef3005131cb79d856beb1ed3
Felipe Pinheiro <flppinheiro@hotmail.com>
2024-06-14 23:07:10 -0300

    creating-migrations-knex

---
+++ src/database/knex/migrations/20240615020106_createUsers.js
@@ -0,0 +1,10 @@
+exports.up = knex => knex.schema.createTable('users', table => {
+    table.increments('id')
+    table.text('name')
+    table.text('email')
+    table.text('password')
+})
+
+exports.down = function(knex) {
+  
+};
